37ab35cb9c78153ff943b85dacdda6f62e4d83c2
Anes2021 <hellalet.anes@gmail.com>
2024-08-17 00:30:33 +0000

    Add Image_Picker and some changes in UI

--- lib/screens/create_blog_screen.dart
+++ lib/screens/create_blog_screen.dart
@@ -1,20 +1,31 @@
 // ignore_for_file: must_be_immutable
 
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mustaqim/core/button_form.dart';
 import 'package:mustaqim/core/colors.dart';
 import 'package:mustaqim/core/text_field_form.dart';
 import 'package:mustaqim/services/crud.dart';
 
-class CreateBlogScreen extends StatelessWidget {
-  CreateBlogScreen({super.key});
+class CreateBlogScreen extends StatefulWidget {
+  const CreateBlogScreen({super.key});
 
+  @override
+  State<CreateBlogScreen> createState() => _CreateBlogScreenState();
+}
+
+class _CreateBlogScreenState extends State<CreateBlogScreen> {
   CrudMethods crudMethods = CrudMethods();
+
   TextEditingController blogTitleController = TextEditingController();
+
   TextEditingController blogDescriptionController = TextEditingController();
+
+  File? image;
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +49,8 @@ class CreateBlogScreen extends StatelessWidget {
                       ),
                       const Spacer(),
                       SizedBox(
-                          height: 175,
-                          width: 175,
+                          height: 160,
+                          width: 160,
                           child: Image.asset(
                             'assets/images/app_icon.png',
                             fit: BoxFit.contain,
@@ -47,35 +58,56 @@ class CreateBlogScreen extends StatelessWidget {
                       const Spacer(),
                     ],
                   ),
-                  Container(
-                    height: 250,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        border:
-                            Border.all(color: ColorsApp.blackColor, width: 2),
-                        color: ColorsApp.greyColor,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.camera_alt_rounded,
-                            size: 50,
-                            color: ColorsApp.whiteColor,
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            "Tap here to add photo",
-                            style: GoogleFonts.aBeeZee(
-                                textStyle: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: ColorsApp.whiteColor)),
-                          ),
-                        ],
-                      ),
+                  InkWell(
+                    onTap: () async {
+                      final imagePicker = ImagePicker();
+                      final pickedImage = await imagePicker.pickImage(
+                          source: ImageSource.gallery);
+                      if (pickedImage != null) {
+                        setState(() {
+                          image = File(pickedImage.path);
+                        });
+                      }
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(color: ColorsApp.blackColor, width: 2),
+                          color: ColorsApp.greyColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: image == null
+                          ? Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const SizedBox(
+                                    height: 50,
+                                  ),
+                                  const Icon(
+                                    Icons.camera_alt_rounded,
+                                    size: 50,
+                                    color: ColorsApp.whiteColor,
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "Tap here to add photo",
+                                    style: GoogleFonts.aBeeZee(
+                                        textStyle: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: ColorsApp.whiteColor)),
+                                  ),
+                                  const SizedBox(
+                                    height: 50,
+                                  ),
+                                ],
+                              ),
+                            )
+                          : ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.file(image!, fit: BoxFit.cover)),
                     ),
                   ),
                   const SizedBox(
@@ -97,6 +129,9 @@ class CreateBlogScreen extends StatelessWidget {
                   ButtonForm(
                     buttonT: 'Upload',
                     function: test,
+                  ),
+                  const SizedBox(
+                    height: 20,
                   ),
                 ],
               ),
